0c4395f146abbdb88bbfc6040629419061ce862e
Corentin Diard <corentin.diard@gmail.com>
2018-05-24 17:03:33 +0200

    change bug on routes and fixed bug method name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "articles", to: "articles#maveille"
+  get "articles", to: "articles#articles"
   resources :articles, only: [ :new, :create, :edit, :update, :destroy]
 end
