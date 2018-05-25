20a262f2661874b8cc6b64f1ca4e7be799371cb3
Corentin Diard <corentin.diard@gmail.com>
2018-05-25 16:10:27 +0200

    cloudinary works

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :paragraphs, inverse_of: :article
   accepts_nested_attributes_for :paragraphs, reject_if: :all_blank, allow_destroy: true
 end
