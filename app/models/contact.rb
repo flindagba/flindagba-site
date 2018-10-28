3dc4442f7e620eb90cc268515d20f5c89761f706
Corentin Diard <corentin.diard@gmail.com>
2018-10-28 14:42:45 +0100

    contact pages done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < MailForm::Base
   append :remote_ip, :user_agent
   attribute :name,      validate: true
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :telephone,     validate: true
   attribute :message
   validates :message,   length: { in: 15..2000 }
 
