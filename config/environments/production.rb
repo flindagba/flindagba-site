b67e4c9e89b5941c17df0c07bdf5a4bad2dfa089
Corentin Diard <corentin.diard@gmail.com>
2018-10-28 16:33:55 +0100

    change url in production for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://www.francine-lindagba-avocat.com" }
+  config.action_mailer.default_url_options = { host: "http://www.francine-lindagba-avocat.herokudns.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
